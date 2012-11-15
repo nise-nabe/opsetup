f9a47d2bbbc23dc02d5294c28962e342abd3507b
watanabe <watanabe@openpne.jp>
2012-11-15 17:16:47 +0900

    fixed not to add dot when base_domain was not set

--- _opdeploy.py
+++ _opdeploy.py
@@ -9,7 +9,11 @@ if __name__ == '__main__':
     conf.read(os.sep.join([os.environ['HOME'], '.openpne' , 'config']))
 
     dirname = os.getcwd().split(os.sep)[-1]
-    domain_name = dirname + '.' + conf.get('web', 'base_domain')
+    base_domain = conf.get('web', 'base_domain')
+    if base_domain != '':
+      base_domain = '.' + base_domain
+
+    domain_name = dirname + base_domain
     target = os.sep.join([conf.get('web', 'sns_base_dir'), domain_name])
     print os.getcwd()
     print target
